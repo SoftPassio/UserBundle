fc695f295c0b307376607fd9726addeb39c7af25
Tymoteusz Ziętek <tymoteusz.zietek@appverk.com>
2019-05-15 11:43:53 +0200

    ver for symfony 4

--- Command/CreateUserCommand.php
+++ Command/CreateUserCommand.php
@@ -5,13 +5,13 @@ namespace AppVerk\UserBundle\Command;
 use AppVerk\Components\Model\UserInterface;
 use AppVerk\UserBundle\Doctrine\UserManager;
 use AppVerk\UserBundle\Entity\User;
-use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Style\SymfonyStyle;
+use Symfony\Component\Console\Command\Command;
 
-class CreateUserCommand extends ContainerAwareCommand
+class CreateUserCommand extends Command
 {
     protected function configure()
     {
